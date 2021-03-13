753ab7cbe6f077a4e663d969d900b2acca8c06fe
Nekoguard <nekoguard@protonmail.com>
2021-03-13 14:32:08 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+
+  if (!matrix) {
+    return [];
+  } else {
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 !== 0) {
+        matrix[i].reverse();
+      }
+    }
+    for (let subArr of matrix) {
+      for (let elem of subArr) {
+        result.push(elem);
+      }
+    }
+    return result;
+  }
 }
